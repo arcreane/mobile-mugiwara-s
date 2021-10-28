faf0b01537277f2446a332b724bc7fd789e1d86c
ghilesalt <ghiles9937@hotmail.fr>
2021-10-28 17:54:26 +0200

    Ecran de connexion

--- lib/LoginDatabase.dart
+++ lib/LoginDatabase.dart
@@ -1,29 +1,30 @@
 import 'package:sqflite/sqflite.dart';
-class LoginDataBase{
+
+class LoginDataBase {
   LoginDataBase._();
 
   static final LoginDataBase instance = LoginDataBase._();
   static Database _database;
 
-  Future<Database> get database async{
+  Future<Database> get database async {
     if (_database != null) return _database;
     _database = await initDB();
     return _database;
   }
 
-  initDB()async {
+  initDB() async {
     WidgetsFlutterBinding.ensureInitialized();
     return await openDatabase(
-      join(await getDatabasesPath(),'login_database.db'),
+      join(await getDatabasesPath(), 'login_database.db'),
       onCreate: (db, version) {
         return db.execute(
-          "CREATE TABLE login(email TEXT PRIMARY KEY,motdepasse TEXT, nom TEXT, prenom TEXT)"
-        );
+            "CREATE TABLE login(email TEXT PRIMARY KEY,motdepasse TEXT, nom TEXT, prenom TEXT)");
       },
       version: 1,
     );
   }
-  void insertRecipe(Login login) async{
+
+  void insertRecipe(Login login) async {
     final Database db = await database;
 
     await db.insert(
