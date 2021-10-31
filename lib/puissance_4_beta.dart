df65146769c054dc9af81f52ebac0660eb81b8d1
gatooon <gl.golitin@gmail.com>
2021-10-31 18:57:53 +0100

    Ajout du deuxième joueur

--- lib/puissance_4_beta.dart
+++ lib/puissance_4_beta.dart
@@ -25,26 +25,36 @@ class titre extends StatefulWidget{
 
 class _titreState extends State<titre>{
     var mot = "test";
-    var column_1 =[false,false,false,false,false,false];
-    var column_2 =[false,false,false,false,false,false];
-    var column_3 =[false,false,false,false,false,false];
-    var column_4 =[false,false,false,false,false,false];
-    var column_5 =[false,false,false,false,false,false];
-    var column_6 =[false,false,false,false,false,false];
-    var column_7 =[false,false,false,false,false,false];
+    var column_1 =[[false, MaterialColor],[false, MaterialColor],[false, MaterialColor],[false, MaterialColor],[false, MaterialColor],[false, MaterialColor]];
+    var column_2 =[[false, MaterialColor],[false, MaterialColor],[false, MaterialColor],[false, MaterialColor],[false, MaterialColor],[false, MaterialColor]];
+    var column_3 =[[false, MaterialColor],[false, MaterialColor],[false, MaterialColor],[false, MaterialColor],[false, MaterialColor],[false, MaterialColor]];
+    var column_4 =[[false, MaterialColor],[false, MaterialColor],[false, MaterialColor],[false, MaterialColor],[false, MaterialColor],[false, MaterialColor]];
+    var column_5 =[[false, MaterialColor],[false, MaterialColor],[false, MaterialColor],[false, MaterialColor],[false, MaterialColor],[false, MaterialColor]];
+    var column_6 =[[false, MaterialColor],[false, MaterialColor],[false, MaterialColor],[false, MaterialColor],[false, MaterialColor],[false, MaterialColor]];
+    var column_7 =[[false, MaterialColor],[false, MaterialColor],[false, MaterialColor],[false, MaterialColor],[false, MaterialColor],[false, MaterialColor]];
+    var player = "player_1";
+    MaterialColor player_color = Colors.green;
 
     void onColumnClic(column){
       setState((){
-        for(int i = 5; i >= 0; i--){
-          if(column[i] == false){
-            column[i] = true;
+        if(player == "player_1"){
+          player = "player_2";
+          player_color = Colors.orange;
+        }else{
+          player = "player_1";
+          player_color = Colors.green;
+        }
+        for(int i = 0; i <= 5; i++){
+          if(column[i][0] == false){
+            column[i][0] = true;     
+            column[i][1] = player_color;
             break;
           }
         }
       });
     }
 
-    grid_case(column_index){
+    grid_case(column_index, column_color){
       if(column_index == false){
         return Container( 
             decoration: BoxDecoration(
@@ -57,7 +67,7 @@ class _titreState extends State<titre>{
         return Container( 
             decoration: BoxDecoration(
               border: Border.all(color: Colors.black),
-              color: Colors.green
+              color: column_color,
             ), 
             width: 50, 
             height: 50,
@@ -82,12 +92,12 @@ class _titreState extends State<titre>{
               onPressed: () => onColumnClic(column),
             ),
           ),
-          grid_case(column[0]), 
-          grid_case(column[1]),
-          grid_case(column[2]),
-          grid_case(column[3]),
-          grid_case(column[4]),
-          grid_case(column[5])
+          grid_case(column[5][0],column[5][1]), 
+          grid_case(column[4][0],column[4][1]),
+          grid_case(column[3][0],column[3][1]),
+          grid_case(column[2][0],column[2][1]),
+          grid_case(column[1][0],column[1][1]),
+          grid_case(column[0][0],column[0][1])
         ];
     }
 
