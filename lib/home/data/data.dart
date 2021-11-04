2009eb3d99a859807204d14e02aec9464a3179de
Alexon1999 <alexon1999@gmail.com>
2021-11-04 18:06:34 +0100

    ajouter home page

---
+++ lib/home/data/data.dart
@@ -0,0 +1,39 @@
+import '../models/Anime.dart';
+
+List<Anime> animes = [
+  Anime(
+      imageUrl:
+          'https://cdn.pixabay.com/photo/2018/02/26/14/22/venice-3183168__340.jpg',
+      nom: 'Naruto',
+      rating: 10,
+      description: 'Visit Venice for an amazing and unforgettable adventure.',
+      genre: 'action'),
+  Anime(
+      imageUrl:
+          'https://cdn.pixabay.com/photo/2018/02/26/14/22/venice-3183168__340.jpg',
+      nom: 'Naruto',
+      rating: 10,
+      description: 'Visit Venice for an amazing and unforgettable adventure.',
+      genre: 'action'),
+  Anime(
+      imageUrl:
+          'https://cdn.pixabay.com/photo/2018/02/26/14/22/venice-3183168__340.jpg',
+      nom: 'Naruto',
+      rating: 10,
+      description: 'Visit Venice for an amazing and unforgettable adventure.',
+      genre: 'action'),
+  Anime(
+      imageUrl:
+          'https://cdn.pixabay.com/photo/2018/02/26/14/22/venice-3183168__340.jpg',
+      nom: 'Naruto',
+      rating: 10,
+      description: 'Visit Venice for an amazing and unforgettable adventure.',
+      genre: 'action'),
+  Anime(
+      imageUrl:
+          'https://cdn.pixabay.com/photo/2018/02/26/14/22/venice-3183168__340.jpg',
+      nom: 'Naruto',
+      rating: 10,
+      description: 'Visit Venice for an amazing and unforgettable adventure.',
+      genre: 'action'),
+];
---
+++ lib/home/screens/details_screen.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+import '../models/Anime.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class DetailsScreen extends StatefulWidget {
+  final Anime? anime;
+
+  DetailsScreen({Key? key, this.anime}) : super(key: key);
+
+  @override
+  _DetailsScreenState createState() => _DetailsScreenState();
+}
+
+class _DetailsScreenState extends State<DetailsScreen> {
+  Text _buildRatingStars(int rating) {
+    String stars = '';
+    for (int i = 0; i < rating; i++) {
+      stars += '⭐ ';
+    }
+    stars.trim();
+    return Text(stars);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Container(
+                height: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      offset: Offset(0.0, 2.0),
+                      blurRadius: 6.0,
+                    ),
+                  ],
+                ),
+                child: Hero(
+                  tag: widget.anime!.imageUrl!,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(30.0),
+                    child: Image(
+                      image: NetworkImage(widget.anime!.imageUrl!),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 40.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.arrow_back),
+                      iconSize: 30.0,
+                      color: Colors.black,
+                      onPressed: () => Navigator.pop(context),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.search),
+                          iconSize: 30.0,
+                          color: Colors.black,
+                          onPressed: () => Navigator.pop(context),
+                        ),
+                        IconButton(
+                          icon: Icon(FontAwesomeIcons.sortAmountDown),
+                          iconSize: 25.0,
+                          color: Colors.black,
+                          onPressed: () => Navigator.pop(context),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                left: 20.0,
+                bottom: 20.0,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      widget.anime!.nom!,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 35.0,
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 1.2,
+                      ),
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          FontAwesomeIcons.locationArrow,
+                          size: 15.0,
+                          color: Colors.white70,
+                        ),
+                        SizedBox(width: 5.0),
+                        Text(
+                          widget.anime!.genre!,
+                          style: TextStyle(
+                            color: Colors.white70,
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                right: 20.0,
+                bottom: 20.0,
+                child: Icon(
+                  Icons.location_on,
+                  color: Colors.white70,
+                  size: 25.0,
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: Container(
+                margin: EdgeInsets.fromLTRB(40.0, 5.0, 20.0, 5.0),
+                height: 170.0,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: Padding(
+                    padding: EdgeInsets.fromLTRB(100.0, 20.0, 20.0, 20.0),
+                    child: Row(children: <Widget>[
+                      Text(
+                        'Rating : ${widget.anime!.rating!}',
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Text(
+                        widget.anime!.description!,
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ]))),
+          ),
+        ],
+      ),
+    );
+  }
+}
